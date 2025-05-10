db1a8b704132cc5ca571670a62f2917886890338
MoElwekel <muhammedelwekel@gmail.com>
2025-05-10 19:42:10 +0300

    learn promises and convert code into promises

--- data/card.js
+++ data/card.js
@@ -65,3 +65,15 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 };
 
 
+export function loadCard(fun){
+  const xhr = new XMLHttpRequest();  
+
+  xhr.addEventListener('load', () => {
+
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET','http://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scrips/checkout.js
+++ scrips/checkout.js
@@ -1,15 +1,74 @@
 import {renderOrderSummury} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
+import {loadCard} from '../data/card.js'
 //import '../data/card-calss.js';
 //import '../data/backend-pactice.js';
 
 
-loadProducts(()=>{
-  // Initial call to render the order summary
-renderOrderSummury();
 
-// Initial call to render the payment summary
-renderPaymentSummary();
+
+Promise.all([
+  new Promise((resolve)=>{
+    console.log('start promise')
+    loadProducts(()=>{
+      console.log('finish loading the prouducts')
+      resolve('val1');
+    });
+  }),
+
+  new Promise((resolve)=>{
+    loadCard(()=>{
+      console.log('finish loading the card');
+      resolve();
+    });
+  }),
+]).then((values)=>{
+  console.log(values);
+  //Initial call to render the order summary
+  renderOrderSummury();
+
+  // Initial call to render the payment summary
+  renderPaymentSummary();
 });
+
+/*
+new Promise((resolve)=>{
+  console.log('start promise')
+  loadProducts(()=>{
+    console.log('finish loading the prouducts')
+    resolve('val1');
+  });
+
+}).then((value)=>{
+  console.log('next step');
+  console.log(value);
+  return new Promise((resolve)=>{
+    loadCard(()=>{
+      console.log('finish loading the card');
+      resolve();
+    });
+  });
+
+}).then(()=>{
+  console.log('next step');
+
+  //Initial call to render the order summary
+  renderOrderSummury();
+
+  // Initial call to render the payment summary
+  renderPaymentSummary();
+  console.log('displaying the page completed');
+})
+*/
+
+// loadProducts(()=>{
+//   loadCard( () => {
+//     // Initial call to render the order summary
+//     renderOrderSummury();
+
+//     // Initial call to render the payment summary
+//     renderPaymentSummary();
+//   }); 
+// });
 
